c2baff2c9eec33fbadf84e14b452d504cc2258d6
Phil Olalere <olalerephilip@gmail.com>
2022-05-08 17:59:25 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Navbar from './components/includes/Navbar';
 import CreateCar from './pages/CreateCar';
 import Home from './pages/Home';
 import ManageCars from './pages/ManageCars';
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import { createBrowserHistory } from "history";
 function App() {
   return (
+    
+    <Router>
     <div className="">
-      <Home/>
-      <CreateCar/>
-      <ManageCars/>
+      <Navbar/>
+        <Routes>
+          <Route path = "/" element={<Home/>}/>
+           
+          <Route path = "/createCar" element={<CreateCar/>}/>
+            
+          <Route path = "/manageCars" element={<ManageCars/>}/>
+            
+          
+        </Routes>
+      
+      
     </div>
+    </Router>
+      
+    
   );
 }
 
